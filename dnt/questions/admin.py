f6e70b10b0bcf0bd262078d3607727addb8b4127
Istrione <naloiko52@yandex.ru>
2023-03-19 22:44:11 +0300

    fix admin

--- dnt/questions/admin.py
+++ dnt/questions/admin.py
@@ -1,38 +1,29 @@
 from django.contrib import admin
-from questions.models import Question, Category, Type, SubType, Answer, QuestionComplaint, Remark
+from .models import Question, Category, Type, SubType, Answer
 
-# # admin.site.register(Question)
-# admin.site.register(Category)
-# admin.site.register(Type)
-# admin.site.register(SubType)
-# admin.site.register(Answer)
-# admin.site.register(QuestionComplaint)
-# admin.site.register(Remark)
 
-@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'category', 'question', 'answer', 'is_validated')
+    list_display = ('question', 'answer')
 
-@admin.register(Category)
+
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name')
+    list_display = ('name',)
 
-@admin.register(Type)
+
 class TypeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name')
+    list_display = ('name',)
 
-@admin.register(SubType)
+
 class SubTypeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'type')
+    list_display = ('name',)
 
-@admin.register(Answer)
+
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'answer', 'subtype')
+    list_display = ('answer',)
 
-@admin.register(Remark)
-class RemarkAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'question', 'text')
 
-@admin.register(QuestionComplaint)
-class QuestionComplaintAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'question', 'text')
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(SubType, SubTypeAdmin)
+admin.site.register(Answer, AnswerAdmin)
