1bb2bed3b6b84581d71dd05a7c4eff0ab2fe3c5d
Istrione <naloiko52@yandex.ru>
2023-03-19 21:59:39 +0300

    updated qes_list

--- dnt/questions/admin.py
+++ dnt/questions/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from questions.models import Question, Category, Type, SubType, Answer, QuestionComplaint, Remark
 
-# Register your models here.
+# # admin.site.register(Question)
+# admin.site.register(Category)
+# admin.site.register(Type)
+# admin.site.register(SubType)
+# admin.site.register(Answer)
+# admin.site.register(QuestionComplaint)
+# admin.site.register(Remark)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'category', 'question', 'answer', 'is_validated')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+
+@admin.register(SubType)
+class SubTypeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'type')
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'answer', 'subtype')
+
+@admin.register(Remark)
+class RemarkAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'question', 'text')
+
+@admin.register(QuestionComplaint)
+class QuestionComplaintAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'question', 'text')
--- dnt/questions/views.py
+++ dnt/questions/views.py
@@ -6,5 +6,10 @@ from questions.models import Question
 class QuestionView(TemplateView):
     template_name = 'questions/qes_list.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = Question.objects.all()
+        return context_data
+
 class AddQuestionsView(TemplateView):
     template_name = 'questions/add_quest.html'
