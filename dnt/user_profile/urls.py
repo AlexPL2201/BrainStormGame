82bd8732b0bece1dc8ebe647e15ce32814a8a176
Roman <rombos1978@gmail.com>
2023-04-02 01:33:07 +0300

    ***

--- dnt/user_profile/urls.py
+++ dnt/user_profile/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 import user_profile.views as u_p
 from user_profile.views import UserDetailView
+
 app_name = 'user_profile'
 
 urlpatterns = [
     path('', u_p.index, name='index'),
-    path('games/', u_p.game_status, name='game_status'),
     path('friends/', u_p.view_friends, name='friends'),
     path('users/<int:pk>/', u_p.UserDetailView.as_view(), name='profile'),
-    path('users_list/', u_p.user_list, name='user_list')
+    path('users_list/', u_p.user_list, name='user_list'),
+    path('friends_list/', u_p.manage_friends, name='manage_friends')
 ]
-
-
-
