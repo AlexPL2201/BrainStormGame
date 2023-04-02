e04fdf3d1cc5dfef12b7cd958cf49aed7aa33f2c
Istrione <naloiko52@yandex.ru>
2023-04-02 10:27:16 +0300

    Added the ability to moderate questions

--- dnt/questions/urls.py
+++ dnt/questions/urls.py
@@ -4,6 +4,8 @@ from questions import views
 app_name = 'questions'
 
 urlpatterns = [
-    path('quest_list/', views.QuestionView.as_view(), name='quest_list'),
-    path('quest_list/add_quest/', views.AddQuestionsView.as_view(), name='add_quest'),
+    path('', views.QuestionView.as_view(), name='quest'),
+    path('add_quest/', views.AddQuestionsView.as_view(), name='add_quest'),
+    path('<int:pk>/delete_quest/', views.QuestionDeleteView.as_view(), name='delete_quest'),
+    path('<int:pk>/update_quest/', views.QuestionUpdateView.as_view(), name='update_quest'),
 ]
