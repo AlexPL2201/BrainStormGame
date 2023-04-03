ee652e421fe2c88b1c7bf1b1585ab951448d96e5
Roman <rombos1978@gmail.com>
2023-04-04 00:05:48 +0300

    finished

--- dnt/user_profile/urls.py
+++ dnt/user_profile/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 import user_profile.views as u_p
 from user_profile.views import UserDetailView
+from user_profile.views import leaderboard
 
 app_name = 'user_profile'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('users/<int:pk>/', u_p.UserDetailView.as_view(), name='profile'),
     path('users_list/', u_p.user_list, name='user_list'),
     path('friends_list/', u_p.manage_friends, name='manage_friends'),
-    path('users/<int:pk>/', u_p.my_games, name='profile')
+    path('users/<int:pk>/', u_p.my_games, name='profile'),
+    path('leaderboard/', u_p.leaderboard, name='leaderboard'),
 ]
